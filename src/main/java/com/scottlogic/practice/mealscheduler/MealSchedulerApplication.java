e1da5b3a97307fa250575e29d8accc6f7c1e7f58
Harry Goode <54807780+hgoody88@users.noreply.github.com>
2022-03-21 10:07:37 +0000

    Init.

---
+++ src/main/java/com/scottlogic/practice/mealscheduler/MealSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.scottlogic.practice.mealscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealSchedulerApplication.class, args);
+	}
+
+}
