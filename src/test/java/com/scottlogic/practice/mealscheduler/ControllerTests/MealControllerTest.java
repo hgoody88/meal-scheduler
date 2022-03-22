86b594037036ed5d83abebf0179cdfd22cc36544
asulkowski <asulkowski@scottlogic.com>
2022-03-22 11:28:10 +0000

    Re-structured a test to use auto-wiring

--- src/test/java/com/scottlogic/practice/mealscheduler/ControllerTests/MealControllerTest.java
+++ src/test/java/com/scottlogic/practice/mealscheduler/ControllerTests/MealControllerTest.java
@@ -4,54 +4,49 @@ import com.scottlogic.practice.mealscheduler.Controllers.MealController;
 import com.scottlogic.practice.mealscheduler.Models.Meal;
 import com.scottlogic.practice.mealscheduler.Repositories.MealRepo;
 import com.scottlogic.practice.mealscheduler.Services.MealService;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
-@RunWith(MockitoJUnitRunner.class)
-@WebMvcTest(controllers = MealController.class)
 public class MealControllerTest {
-    @Mock
-    private MealRepo mealRepo;
 
-    @InjectMocks
-    private MealService mealService;
+    @MockBean
+    MealRepo mealRepo;
+
+    @Autowired
+    MealService mealService;
+
+    @Autowired
+    MealController mealController;
+
+    private final List<Meal> testMeals = List.of(
+            new Meal(UUID.randomUUID(),
+                    "Curry",
+                    LocalDateTime.of(2022, 2, 2, 12, 32),
+                    "User Two"),
+            new Meal(UUID.randomUUID(),
+                    "Pasta",
+                    LocalDateTime.of(2022, 1, 1, 10, 30),
+                    "User One"));
+
 
     @Test
-    public void GetAllMeals_ShouldReturnListOfMeals() throws Exception {
-        //Arrange
-        MealController mealController = new MealController(mealService);
-
-        var meals = new ArrayList<Meal>();
-        meals.add(new Meal(
-                UUID.randomUUID(),
-                "Curry",
-                LocalDateTime.of(2022, 2, 2, 12, 32),
-                "User Two"));
-        meals.add(new Meal(
-                UUID.randomUUID(),
-                "Pasta",
-                LocalDateTime.of(2022, 1, 1, 10, 30),
-                "User One"));
-
-        when(mealRepo.findAll()).thenReturn(meals);
+    public void GetAllMeals_ShouldReturnListOfMeals() {
+        when(mealRepo.findAll()).thenReturn(testMeals);
 
         //Act
         var result = mealController.GetAllMeals();
 
         //Assert
-        assertEquals(result, meals);
+        assertEquals(result, testMeals);
     }
 }
